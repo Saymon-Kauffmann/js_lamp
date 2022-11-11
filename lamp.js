08e6ec23eaa4671fc7911bf4edb606752e87dfb7
Saymon_Kauffmann <101084320+Saymon-Kauffmann@users.noreply.github.com>
2022-11-10 23:39:12 -0300

    final version of this lamp

--- lamp.js
+++ lamp.js
@@ -2,14 +2,28 @@ const turn_on = document.getElementById('on');
 const turn_off = document.getElementById('off');
 const lamp = document.getElementById('lamp');
 
+turn_on.addEventListener('click', lamp_on);
+turn_off.addEventListener('click', lamp_off);
+lamp.addEventListener('mouseover', lamp_on);
+lamp.addEventListener('mouseleave', lamp_off);
+lamp.addEventListener('dblclick', broke);
+
 function lamp_on () {
-    lamp.src = './images/turned_on.jpg';
+    if (!brkd_lamp ()) {
+        lamp.src = './images/turned_on.jpg';
+    }
 }
 
 function lamp_off () {
-    lamp.src = './images/turned_off.jpg';
+    if (!brkd_lamp ()) {
+        lamp.src = './images/turned_off.jpg';
+    }
 }
 
-turn_on.addEventListener('click', lamp_on);
+function broke () {
+    lamp.src = './images/broked.jpg';
+}
 
-turn_off.addEventListener('click', lamp_off);
+function brkd_lamp () {
+    return lamp.src.indexOf('broked') > -1;
+}
