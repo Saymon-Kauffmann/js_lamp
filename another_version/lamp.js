740beb8d7437b03c14039595e7e393a57253236e
Saymon_Kauffmann <101084320+Saymon-Kauffmann@users.noreply.github.com>
2022-11-11 02:42:48 -0300

    normal functions to arrow functions

--- another_version/lamp.js
+++ another_version/lamp.js
@@ -4,27 +4,24 @@ const lamp = document.getElementById('lamp');
 lamp.addEventListener('mouseover', lamp_on);
 lamp.addEventListener('mouseleave', lamp_off);
 turnOnOff.addEventListener('click', offOn);
-lamp.addEventListener('dblclick', broke);
+lamp.addEventListener('dblclick', () => lamp.src = './images/broked.jpg');
 
+const brkd_lamp = () => lamp.src.indexOf('broked') > -1;
+
+const ifLampOn = () => lamp.src.indexOf('turned_on') > -1;
+
+const ifLampOff = () => lamp.src.indexOf('turned_off') > -1;
 
 function lamp_on () {
-    if (!brkd_lamp () && ifLampOff ()) {
+    if (!brkd_lamp ()) {
         lamp.src = './images/turned_on.jpg';
     }
 }
 
 function lamp_off () {
-    if (!brkd_lamp () && ifLampOn ()) {
+    if (!brkd_lamp ()) {
         lamp.src = './images/turned_off.jpg';
     }
-}
-
-function broke () {
-    lamp.src = './images/broked.jpg';
-}
-
-function brkd_lamp () {
-    return lamp.src.indexOf('broked') > -1;
 }
 
 function offOn () {
@@ -37,10 +34,4 @@ function offOn () {
     
 }
 
-function ifLampOn () {
-    return lamp.src.indexOf('turned_on') > -1;
-}
 
- function ifLampOff () {
-    return lamp.src.indexOf('turned_off') > -1;
- }
